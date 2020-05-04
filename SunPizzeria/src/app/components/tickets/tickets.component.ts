8e9b37649cbdc403e442670cffaf5d9f3b861c0b
unknown <wsd81_2@hotmail.com>
2020-05-04 16:36:30 -0400

    add site design

--- SunPizzeria/src/app/components/tickets/tickets.component.ts
+++ SunPizzeria/src/app/components/tickets/tickets.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketsComponent implements OnInit {
 
+  //tickets:Ticket[];
   constructor() { }
 
   ngOnInit(): void {
   }
-
+ 
 }
