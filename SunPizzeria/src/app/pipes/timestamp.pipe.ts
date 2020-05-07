8abed29106bff1cb854bdf6f56101ddb7c9458f9
Wael <45563166+Dawoudw@users.noreply.github.com>
2020-05-06 20:09:55 -0400

    Add files via upload

--- SunPizzeria/src/app/pipes/timestamp.pipe.ts
+++ SunPizzeria/src/app/pipes/timestamp.pipe.ts
@@ -1,12 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {DateFormat} from 'src/app/models/DateFormat';
+ 
 
 @Pipe({
-  name: 'timestamp'
+  name: 'dateTimeFormat'
 })
-export class TimestampPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+export class TimestampPipe extends DatePipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    return super.transform(value, DateFormat.DATE_TIME_FMT);
   }
 
 }
