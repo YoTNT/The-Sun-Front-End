9045c8bf6610bb26b3cb75442a698f06e9193256
Wael <45563166+Dawoudw@users.noreply.github.com>
2020-05-06 20:10:45 -0400

    Add files via upload

--- SunPizzeria/src/app/services/ticket.service.ts
+++ SunPizzeria/src/app/services/ticket.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Ticket } from '../models/Ticket';
+import { promise } from 'protractor';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketService {
 
-  constructor() { }
+  ticketlist: Array<Ticket>;
+  ticket: Ticket;
+  constructor(private httprequest: HttpClient) { }
+
+  async registredUserTickets(userid: number): Promise<Array<Ticket>> {
+
+    let response: Promise<any> = this.httprequest.get('http://localhost:9000/tickets/usertickets/' + userid).toPromise();
+ 
+    return response;
+
+
+  }
+
+
+
+  async function (userid: number) {
+    let response = await fetch('http://localhost:9000/tickets/usertickets/' + userid)
+    if (response.ok) {
+        let body = await response.text();
+        //alert(body);
+        let jsn = JSON.parse(body);
+        //    alert(jsn);
+       
+     
+       
+    }
+  
+}
+
+
+
+
+
+
+
+
+
 }
