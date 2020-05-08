ed3bb18573f33a77c22b9aa2ff65b7534fae5965
YoTNT <pikaqiujian@gmail.com>
2020-05-07 21:06:43 -0400

    update implementation of login including dialog commponent and pass user information to ticket component

--- SunPizzeria/src/app/components/tickets/tickets.component.ts
+++ SunPizzeria/src/app/components/tickets/tickets.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Ticket } from 'src/app/models/Ticket';
 import { TicketService } from 'src/app/services/ticket.service';
-
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-tickets',
@@ -11,16 +12,24 @@ import { TicketService } from 'src/app/services/ticket.service';
 export class TicketsComponent implements OnInit {
 
   tickets:Array<Ticket>;
+  user:User = new User();
 
   ticket: any;
-  constructor(private ticketServ: TicketService) { }
+  constructor(
+    private ticketServ: TicketService,
+    private userServ: UserService
+    ) { 
+      this.user = userServ.userInfo;
+    }
   columns: string[][] = [];
   
   ngOnInit(): void {
+    console.log("Trying to run ngOnInit in Ticket component!");
     this.getUserTickets();
   }
   async getUserTickets(): Promise<Array<Ticket>> {
-    this.tickets = await this.ticketServ.registredUserTickets(3);
+    // cousole.log(this.user);
+    this.tickets = await this.ticketServ.registredUserTickets(this.user.userId);
    
      return this.tickets;
 
