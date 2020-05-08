b8c7878130672db2309c2dee45a11dc8f3e7d214
sandraroy3 <62570600+sandraroy3@users.noreply.github.com>
2020-05-08 14:28:37 -0400

    Added register compt

--- SunPizzeria/src/app/components/register/register.component.ts
+++ SunPizzeria/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service'
+import { User } from 'src/app/models/User';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-register',
@@ -7,8 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userservice:UserService, private router:Router) { } //TODO put private userservice:UserService
 
+  user:User;
+  username:string;
+  newpassword:string;
+  confirmpassword:string;
+
+  users:any;
+
+  msg:string;
+
+  redirectToCustomer(){
+    this.router.navigate(['customer']);
+  }
+
+  redirectToEmployee(){
+    this.router.navigate(['employee']);
+  }
+
+  async RegisterUser():Promise<User>{
+
+    if(this.newpassword===this.confirmpassword){
+      this.user = await this.userservice.createUser(this.username, this.newpassword);
+      this.userservice.userInfo = this.user;
+      this.redirectToCustomer();
+      console.log(this.user);
+    }
+    else{
+      this.msg="Sorry! Passwords have to match.";
+    }
+    return this.user;
+  }
+
+  
   ngOnInit(): void {
   }
 
