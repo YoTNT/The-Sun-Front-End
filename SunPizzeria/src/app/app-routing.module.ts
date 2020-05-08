8abafc03f2edb38c6ca035541505cf6eb32d40e3
sandraroy3 <62570600+sandraroy3@users.noreply.github.com>
2020-05-08 14:32:29 -0400

    Update app-routing.module.ts

--- SunPizzeria/src/app/app-routing.module.ts
+++ SunPizzeria/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+import {RegisterComponent} from './components/register/register.component';
 
 const routes: Routes = [
   {path: "login", component:LoginComponent},
   {path: "customer", component:CustomerComponent},
-  {path: "employee", component:EmployeeComponent}
+  {path: "employee", component:EmployeeComponent},
+  {path: "registeruser", component:RegisterComponent}
 
 ];
 
